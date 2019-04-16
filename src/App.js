d6e36d7e1549857e718eee2f9e4db3b7251722d6
Malvin Guo <malvin.guo@nyumc.org>
2019-04-16 10:09:33 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './App.css';
+import '../node_modules/video-react/dist/video-react.css';
+import HookTest from './HookTest'
+import VideoTest from './VideoTest'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: ['hello', 'world', 'click', 'me']};
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ]);
+    this.setState({items: newItems});
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  }
+
   render() {
+    const items = this.state.items.map((item, i) => (
+      <div key={item} onClick={() => this.handleRemove(i)}>
+        {item}
+      </div>
+    ));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={1000}>
+
+          {items}
+        </ReactCSSTransitionGroup>
+        <HookTest />
+        <div className="video-container">
+          <VideoTest />
+        </div>
+
+        <video width="400" controls autoplay>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            type="video/mp4" />
+        </video>
+
       </div>
     );
   }
---
+++ src/HookTest.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function HookTest () {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </>
+  );
+}
+
+export default HookTest
